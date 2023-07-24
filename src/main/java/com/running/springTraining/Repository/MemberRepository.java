7d6b38126db7394794f311d4a3c7a13390261fb1
btjdghks2 <btjdghks2@gmail.com>
2023-07-25 00:20:25 +0900

    eneity class coding

--- src/main/java/com/running/springTraining/Repository/MemberRepository.java
+++ src/main/java/com/running/springTraining/Repository/MemberRepository.java
@@ -1,7 +1,21 @@
 package com.running.springTraining.Repository;
 
 import com.running.springTraining.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
 }
--- src/main/java/com/running/springTraining/domain/Cart.java
+++ src/main/java/com/running/springTraining/domain/Cart.java
@@ -32,7 +32,7 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
     private List<Product> products = new ArrayList<>();
 
-
+//
 //    @OneToMany(mappedBy = "cart")
 //    private List<Comment> comments = new ArrayList<>();
 
